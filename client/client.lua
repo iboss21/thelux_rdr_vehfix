5aa48c31c6a9e68233edb0b745b9c078dc07c5c2
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-07-11 20:03:49 -0500

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,6 +1,5 @@
 local framework = Config.Framework
 local notificationSystem = Config.NotificationSystem
-local targetSystem = Config.TargetSystem
 
 local function notify(message)
     if notificationSystem == 'ox_lib' then
@@ -8,7 +7,7 @@ local function notify(message)
     elseif notificationSystem == 'mythic_notify' then
         exports.mythic_notify:SendAlert('inform', message)
     elseif notificationSystem == 'esx' then
-        ESX.ShowNotification(message)
+        TriggerClientEvent('esx:showNotification', -1, message)
     end
 end
 
@@ -35,6 +34,22 @@ local function spawnVehicle(vehicleModel, spawnLocation)
     logAndNotify('Vehicle spawned: ' .. vehicleModel)
 end
 
+local function spawnBoat(boatModel, spawnLocation)
+    if not IsModelInCdimage(boatModel) or not IsModelABoat(boatModel) then
+        return
+    end
+
+    RequestModel(boatModel)
+    while not HasModelLoaded(boatModel) do
+        Citizen.Wait(1)
+    end
+
+    local boat = CreateVehicle(boatModel, spawnLocation.x, spawnLocation.y, spawnLocation.z, 0.0, true, false)
+    SetModelAsNoLongerNeeded(boatModel)
+
+    logAndNotify('Boat spawned: ' .. boatModel)
+end
+
 local function initializeFramework()
     if framework == 'qbr' then
         QBRCore = exports['qbr-core']:GetCoreObject()
@@ -42,6 +57,11 @@ local function initializeFramework()
         RegisterNetEvent('qbr:vehicleSpawned')
         AddEventHandler('qbr:vehicleSpawned', function(vehicle)
             spawnVehicle(vehicle, Config.Vehicle.SpawnLocations[1])
+        end)
+
+        RegisterNetEvent('qbr:boatSpawned')
+        AddEventHandler('qbr:boatSpawned', function(boat)
+            spawnBoat(boat, Config.Boat.SpawnLocations[1])
         end)
     elseif framework == 'vorp' then
         VORPcore = {}
@@ -53,12 +73,22 @@ local function initializeFramework()
         AddEventHandler('vorp:vehicleSpawned', function(vehicle)
             spawnVehicle(vehicle, Config.Vehicle.SpawnLocations[1])
         end)
+
+        RegisterNetEvent('vorp:boatSpawned')
+        AddEventHandler('vorp:boatSpawned', function(boat)
+            spawnBoat(boat, Config.Boat.SpawnLocations[1])
+        end)
     elseif framework == 'rsg' then
         RSGCore = exports['rsg-core']:GetCoreObject()
 
         RegisterNetEvent('rsg:vehicleSpawned')
         AddEventHandler('rsg:vehicleSpawned', function(vehicle)
             spawnVehicle(vehicle, Config.Vehicle.SpawnLocations[1])
+        end)
+
+        RegisterNetEvent('rsg:boatSpawned')
+        AddEventHandler('rsg:boatSpawned', function(boat)
+            spawnBoat(boat, Config.Boat.SpawnLocations[1])
         end)
     elseif framework == 'theluxempire' then
         TheLuxEmpireCore = exports['theluxempire-core']:GetCoreObject()
@@ -67,32 +97,11 @@ local function initializeFramework()
         AddEventHandler('theluxempire:vehicleSpawned', function(vehicle)
             spawnVehicle(vehicle, Config.Vehicle.SpawnLocations[1])
         end)
-    end
-end
 
-local function initializeTargetSystem()
-    if targetSystem == 'qtarget' then
-        exports.qtarget:AddTargetModel(Config.Vehicle.AllowedVehicles, {
-            options = {
-                {
-                    event = "vehicle:spawn",
-                    icon = "fas fa-car",
-                    label = "Spawn Vehicle"
-                }
-            },
-            distance = 2.5
-        })
-    elseif targetSystem == 'ox_target' then
-        exports.ox_target:addModel(Config.Vehicle.AllowedVehicles, {
-            {
-                name = 'spawn_vehicle',
-                label = 'Spawn Vehicle',
-                icon = 'fas fa-car',
-                onSelect = function()
-                    TriggerEvent('vehicle:spawn')
-                end
-            }
-        })
+        RegisterNetEvent('theluxempire:boatSpawned')
+        AddEventHandler('theluxempire:boatSpawned', function(boat)
+            spawnBoat(boat, Config.Boat.SpawnLocations[1])
+        end)
     end
 end
 
@@ -101,4 +110,3 @@ if Config.VersionCheck then
 end
 
 initializeFramework()
-initializeTargetSystem()
