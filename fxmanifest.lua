5d09afcd6d9f03c4bf5e44a1bfeb8534e6fd4eef
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-07-11 20:20:25 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,33 @@
 fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
-author 'theluxempire - iBoss21'
-description 'Advanced Vehicle Fix for RedM, compatible with multiple frameworks and notification/target systems'
+description 'TheLux RDR Vehicle Fix'
 version '1.0.0'
 
-shared_script 'config.lua'
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@rsg-core/shared/locale.lua',
+    'locales/en.lua', -- Change to your language
+    'config.lua',
+}
 
-client_script 'client/client.lua'
-server_script 'server/server.lua'
+client_scripts {
+    'client/client.lua',
+    'client/npcs.lua',
+    'config.lua',
+}
 
--- Ensure the resource name is correct
-local resource_name = GetCurrentResourceName()
-if resource_name ~= 'thelux_rdr_vehfix' then
-    print('This resource must be named "thelux_rdr_vehfix" for it to work correctly.')
-    return
-end
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/server.lua',
+    'config.lua',
+}
+
+dependencies {
+    'rsg-core',
+    'rsg-target',
+    'ox_lib'
+}
+
+lua54 'yes'
